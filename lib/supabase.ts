5704509163c29fb27983e07f9e06516882043c16
ankitfjjiu <ankitkumar786958@gmail.com>
2026-02-19 20:32:09 +0530

    Update Supabase URL and key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://myuqhthjcmuamiftwxyb.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_YcrVKkzLUCi8auCHswcfqw_Vid0GWuF';
+const SUPABASE_URL = 'https://libqgqvmjkwzrzsntqos.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_2GrnGLULuGzWbBtKKIn7qQ_EZJZqwC5';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
