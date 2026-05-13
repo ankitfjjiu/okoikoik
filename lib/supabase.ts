d5408ad85a0302f9b9951b2568206b1364a73f00
ankitfjjiu <ankitkumar786958@gmail.com>
2026-05-13 09:49:11 +0530

    Update Supabase project URLs and keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,8 +4,8 @@ const projects = [
   { id: 1, url: 'https://pzfvdryaqxwhrtfhpood.supabase.co', key: 'sb_publishable_R5cblIRgwcT0vnTTAY6_1Q_RzHM044n' },
   { id: 2, url: 'https://ohyszjcmnnoqpaegozxq.supabase.co', key: 'sb_publishable_GPeydA56Zvp3IgAxHlzy9g_c51IzpTG' },
   { id: 3, url: 'https://ptpwbxqvxdyzmgthvstj.supabase.co', key: 'sb_publishable_b3uo7jjjEyLGiJ4prGXV7A_fp8Kd7lJ' },
-  { id: 4, url: 'https://dmbgokkudhmtthfisbop.supabase.co', key: 'sb_publishable_U0yEQrTn0kCleNvBbBce6A_R6gr7qk6' },
-  { id: 5, url: 'https://qpszmpuulfjaczzmqfpn.supabase.co', key: 'sb_publishable_NymaC6i9FhTHueDxSpBcGw_Mc5IR5yA' },
+  { id: 4, url: 'https://awqltpjubohmckkderzt.supabase.co', key: 'sb_publishable_PVV0YwOvwy5WioMqNk9h8A_w_b0f1mF' },
+  { id: 5, url: 'https://wztpmghepkjldbrvipie.supabase.co', key: 'sb_publishable_M0cfbmeF3Ms1yVxtafB_YA_3vzdy0O1' },
   { id: 6, url: 'https://ajkqczrgcjsikcwjjxgy.supabase.co', key: 'sb_publishable_xM3qDr_jtNsuOecnxuM6pQ_H3Xp_0X6' },
   { id: 7, url: 'https://ahchpedtmdvuexvaixas.supabase.co', key: 'sb_publishable_Og8xZsKtq28C4mLiP012KQ_DfdOMckq' }
 ];
